fce0ceea60732385be003508627ccc83a2a2cd17
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-01-04 21:44:00 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,18 @@
 import streamlit as st
 from PIL import Image
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Home page function
 def home_page():
