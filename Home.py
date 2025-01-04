db8e568ecdc2c531395c5a86931f5e758caa50d5
Lior Ganon <73948396+liorgan93@users.noreply.github.com>
2025-01-04 21:46:17 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,18 +1,12 @@
 import streamlit as st
 from PIL import Image
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 # Home page function
 def home_page():
